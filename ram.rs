72a01dd9535e5205ec353af7e2869a3d8ad169a8
Andreas Neuhaus <zargony@zargony.com>
2013-06-25 18:33:59 +0200

    Fixed RAM size problems

--- ram.rs
+++ ram.rs
@@ -7,22 +7,21 @@ pub struct Ram<ADDR> {
 }
 
 impl<ADDR: Int> Ram<ADDR> {
-	pub fn new (size: ADDR) -> ~Ram<ADDR> {
-		let s: uint = num::cast(size);
-		~Ram { data: vec::from_elem(s, 0) }
+	pub fn new (size: uint) -> ~Ram<ADDR> {
+		~Ram { data: vec::from_elem(size, 0) }
 	}
 }
 
 impl<ADDR: Int> Addressable<ADDR, u8> for Ram<ADDR> {
 	pub fn get (&self, addr: ADDR) -> u8 {
 		let i: uint = num::cast(addr);
-		if i > self.data.len()-1 { fail!("memory: read beyond memory bounds (%? > %?)", i, self.data.len()-1); }
+		if i >= self.data.len() { fail!("memory: read beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
 		self.data[i]
 	}
 
 	pub fn set (&mut self, addr: ADDR, data: u8) {
 		let i: uint = num::cast(addr);
-		if i > self.data.len()-1 { fail!("memory: write beyond memory bounds (%? > %?)", i, self.data.len()-1); }
+		if i >= self.data.len() { fail!("memory: write beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
 		self.data[i] = data;
 	}
 }
