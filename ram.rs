c27193329e2ca19c50e73f24bd2b0982dd0444f9
Andreas Neuhaus <zargony@zargony.com>
2013-06-25 18:58:02 +0200

    Fixed ram failure messages

--- ram.rs
+++ ram.rs
@@ -6,23 +6,23 @@ pub struct Ram<ADDR> {
 	data: ~[u8],
 }
 
-impl<ADDR: Int> Ram<ADDR> {
-	pub fn new (size: uint) -> ~Ram<ADDR> {
-		~Ram { data: vec::from_elem(size, 0) }
-	}
-}
-
 impl<ADDR: Int> Addressable<ADDR, u8> for Ram<ADDR> {
 	pub fn get (&self, addr: ADDR) -> u8 {
 		let i: uint = num::cast(addr);
-		if i >= self.data.len() { fail!("memory: read beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
+		if i >= self.data.len() { fail!("ram: read beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
 		self.data[i]
 	}
 
 	pub fn set (&mut self, addr: ADDR, data: u8) {
 		let i: uint = num::cast(addr);
-		if i >= self.data.len() { fail!("memory: write beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
+		if i >= self.data.len() { fail!("ram: write beyond memory bounds ($%x >= $%x)", i, self.data.len()); }
 		self.data[i] = data;
+	}
+}
+
+impl<ADDR: Int> Ram<ADDR> {
+	pub fn new (size: uint) -> ~Ram<ADDR> {
+		~Ram { data: vec::from_elem(size, 0) }
 	}
 }
 
