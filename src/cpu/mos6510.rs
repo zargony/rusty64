344796c128aaaf36d321062f34729f93e7749de5
Andreas Neuhaus <zargony@zargony.com>
2013-12-30 16:57:23 +0100

    Add 6510 CPU based on the 6502 CPU

---
+++ src/cpu/mos6510.rs
@@ -0,0 +1,31 @@
+use mem::Addressable;
+use super::cpu::CPU;
+use super::mos6502::Mos6502;
+
+/// The MOS65010 processor
+pub struct Mos6510 {
+	priv cpu: Mos6502,						// Core CPU is a MOS6502
+	priv port_ddr: u8,						// CPU port data direction register
+	priv port_dat: u8,						// CPU port data register
+}
+
+impl Mos6510 {
+	/// Create a new MOS6510 processor
+	pub fn new () -> Mos6510 {
+		// TODO: addresses $0000 (data direction) and $0001 (data) are hardwired for the processor I/O port
+		Mos6510 { cpu: Mos6502::new(), port_ddr: 0, port_dat: 0 }
+	}
+}
+
+impl CPU<u16> for Mos6510 {
+	/// Reset the CPU
+	fn reset<M: Addressable<u16>> (&mut self, mem: &M) {
+		self.cpu.reset(mem);
+	}
+
+	/// Do one step (execute the next instruction). Returns the number of
+	/// cycles the instruction needed
+	fn step<M: Addressable<u16>> (&mut self, mem: &mut M) -> uint {
+		self.cpu.step(mem)
+	}
+}
