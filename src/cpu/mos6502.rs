b80b64021ed762f7f0aac00374e383dd13b83f24
Andreas Neuhaus <zargony@zargony.com>
2013-12-31 16:26:51 +0100

    Implement 6502 instruction and operand parsing

--- src/cpu/mos6502.rs
+++ src/cpu/mos6502.rs
@@ -8,6 +8,7 @@ use super::cpu::CPU;
 // Processor bugs and caveats : http://www.textfiles.com/apple/6502.bugs.txt
 
 /// Processor instructions
+#[deriving(Eq)]
 enum Instruction {
 	// Load/store operations
 	LDA, LDX, LDY, STA, STX, STY,
@@ -53,6 +54,7 @@ impl Instruction {
 }
 
 /// Instruction operand with different addressing modes
+#[deriving(Eq)]
 enum Operand {
 	Implied,							// OPC				operand implied
 	Immediate(u8),						// OPC #$BB			operand is value $BB
@@ -132,6 +134,177 @@ impl Mos6502 {
 		let val = mem.get_le(self.pc);
 		self.pc += Primitive::bytes(None::<T>) as u16;
 		val
+	}
+
+	/// Parse next instruction and advance PC. Returns number of cycles, instruction and operand
+	fn next_instruction<M: Addressable<u16>> (&mut self, mem: &M) -> Option<(uint, Instruction, Operand)> {
+		let opcode: u8 = self.next(mem);
+		Some(match opcode {
+			// Load/store operations
+			0xa9 => (2, LDA, Immediate(self.next(mem))),
+			0xa5 => (3, LDA, ZeroPage(self.next(mem))),
+			0xb5 => (4, LDA, ZeroPageIndexedWithX(self.next(mem))),
+			0xad => (4, LDA, Absolute(self.next(mem))),
+			0xbd => (4, LDA, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0xb9 => (4, LDA, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0xa1 => (6, LDA, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0xb1 => (5, LDA, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0xa2 => (2, LDX, Immediate(self.next(mem))),
+			0xa6 => (3, LDX, ZeroPage(self.next(mem))),
+			0xb6 => (4, LDX, ZeroPageIndexedWithY(self.next(mem))),
+			0xae => (4, LDX, Absolute(self.next(mem))),
+			0xbe => (4, LDX, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0xa0 => (2, LDY, Immediate(self.next(mem))),
+			0xa4 => (3, LDY, ZeroPage(self.next(mem))),
+			0xb4 => (4, LDY, ZeroPageIndexedWithX(self.next(mem))),
+			0xac => (4, LDY, Absolute(self.next(mem))),
+			0xbc => (4, LDY, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0x85 => (3, STA, ZeroPage(self.next(mem))),
+			0x95 => (4, STA, ZeroPageIndexedWithX(self.next(mem))),
+			0x8d => (4, STA, Absolute(self.next(mem))),
+			0x9d => (5, STA, AbsoluteIndexedWithX(self.next(mem))),
+			0x99 => (5, STA, AbsoluteIndexedWithY(self.next(mem))),
+			0x81 => (6, STA, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0x91 => (6, STA, ZeroPageIndirectIndexedWithY(self.next(mem))),
+			0x86 => (3, STX, ZeroPage(self.next(mem))),
+			0x96 => (4, STX, ZeroPageIndexedWithY(self.next(mem))),
+			0x8e => (4, STX, Absolute(self.next(mem))),
+			0x84 => (3, STY, ZeroPage(self.next(mem))),
+			0x94 => (4, STY, ZeroPageIndexedWithX(self.next(mem))),
+			0x8c => (4, STY, Absolute(self.next(mem))),
+			// Register transfers
+			0xaa => (2, TAX, Implied),
+			0xa8 => (2, TAY, Implied),
+			0x8a => (2, TXA, Implied),
+			0x98 => (2, TYA, Implied),
+			// Stack operations
+			0xba => (2, TSX, Implied),
+			0x9a => (2, TXS, Implied),
+			0x48 => (3, PHA, Implied),
+			0x08 => (3, PHP, Implied),
+			0x68 => (4, PLA, Implied),
+			0x28 => (4, PLP, Implied),
+			// Logical
+			0x29 => (2, AND, Immediate(self.next(mem))),
+			0x25 => (3, AND, ZeroPage(self.next(mem))),
+			0x35 => (4, AND, ZeroPageIndexedWithX(self.next(mem))),
+			0x2d => (4, AND, Absolute(self.next(mem))),
+			0x3d => (4, AND, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0x39 => (4, AND, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0x21 => (6, AND, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0x31 => (5, AND, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0x49 => (2, EOR, Immediate(self.next(mem))),
+			0x45 => (3, EOR, ZeroPage(self.next(mem))),
+			0x55 => (4, EOR, ZeroPageIndexedWithX(self.next(mem))),
+			0x4d => (4, EOR, Absolute(self.next(mem))),
+			0x5d => (4, EOR, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0x59 => (4, EOR, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0x41 => (6, EOR, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0x51 => (5, EOR, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0x09 => (2, ORA, Immediate(self.next(mem))),
+			0x05 => (3, ORA, ZeroPage(self.next(mem))),
+			0x15 => (4, ORA, ZeroPageIndexedWithX(self.next(mem))),
+			0x0d => (4, ORA, Absolute(self.next(mem))),
+			0x1d => (4, ORA, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0x19 => (4, ORA, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0x01 => (6, ORA, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0x11 => (5, ORA, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0x24 => (3, BIT, ZeroPage(self.next(mem))),
+			0x2c => (4, BIT, Absolute(self.next(mem))),
+			// Arithmetic
+			0x69 => (2, ADC, Immediate(self.next(mem))),
+			0x65 => (3, ADC, ZeroPage(self.next(mem))),
+			0x75 => (4, ADC, ZeroPageIndexedWithX(self.next(mem))),
+			0x6d => (4, ADC, Absolute(self.next(mem))),
+			0x7d => (4, ADC, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0x79 => (4, ADC, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0x61 => (6, ADC, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0x71 => (5, ADC, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0xe9 => (2, SBC, Immediate(self.next(mem))),
+			0xe5 => (3, SBC, ZeroPage(self.next(mem))),
+			0xf5 => (4, SBC, ZeroPageIndexedWithX(self.next(mem))),
+			0xed => (4, SBC, Absolute(self.next(mem))),
+			0xfd => (4, SBC, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0xf9 => (4, SBC, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0xe1 => (6, SBC, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0xf1 => (5, SBC, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0xc9 => (2, CMP, Immediate(self.next(mem))),
+			0xc5 => (3, CMP, ZeroPage(self.next(mem))),
+			0xd5 => (4, CMP, ZeroPageIndexedWithX(self.next(mem))),
+			0xcd => (4, CMP, Absolute(self.next(mem))),
+			0xdd => (4, CMP, AbsoluteIndexedWithX(self.next(mem))),				// +1 cycle if page crossed
+			0xd9 => (4, CMP, AbsoluteIndexedWithY(self.next(mem))),				// +1 cycle if page crossed
+			0xc1 => (6, CMP, ZeroPageIndexedWithXIndirect(self.next(mem))),
+			0xd1 => (5, CMP, ZeroPageIndirectIndexedWithY(self.next(mem))),		// +1 cycle if page crossed
+			0xe0 => (2, CPX, Immediate(self.next(mem))),
+			0xe4 => (3, CPX, ZeroPage(self.next(mem))),
+			0xec => (4, CPX, Absolute(self.next(mem))),
+			0xc0 => (2, CPY, Immediate(self.next(mem))),
+			0xc4 => (3, CPY, ZeroPage(self.next(mem))),
+			0xcc => (4, CPY, Absolute(self.next(mem))),
+			// Increments & decrements
+			0xe6 => (5, INC, ZeroPage(self.next(mem))),
+			0xf6 => (6, INC, ZeroPageIndexedWithX(self.next(mem))),
+			0xee => (6, INC, Absolute(self.next(mem))),
+			0xfe => (7, INC, AbsoluteIndexedWithX(self.next(mem))),
+			0xe8 => (2, INX, Implied),
+			0xc8 => (2, INY, Implied),
+			0xc6 => (5, DEC, ZeroPage(self.next(mem))),
+			0xd6 => (6, DEC, ZeroPageIndexedWithX(self.next(mem))),
+			0xce => (6, DEC, Absolute(self.next(mem))),
+			0xde => (7, DEC, AbsoluteIndexedWithX(self.next(mem))),
+			0xca => (2, DEX, Implied),
+			0x88 => (2, DEY, Implied),
+			// Shifts
+			0x0a => (2, ASL, Accumulator),
+			0x06 => (5, ASL, ZeroPage(self.next(mem))),
+			0x16 => (6, ASL, ZeroPageIndexedWithX(self.next(mem))),
+			0x0e => (6, ASL, Absolute(self.next(mem))),
+			0x1e => (7, ASL, AbsoluteIndexedWithX(self.next(mem))),
+			0x4a => (2, LSR, Accumulator),
+			0x46 => (5, LSR, ZeroPage(self.next(mem))),
+			0x56 => (6, LSR, ZeroPageIndexedWithX(self.next(mem))),
+			0x4e => (6, LSR, Absolute(self.next(mem))),
+			0x5e => (7, LSR, AbsoluteIndexedWithX(self.next(mem))),
+			0x2a => (2, ROL, Accumulator),
+			0x26 => (5, ROL, ZeroPage(self.next(mem))),
+			0x36 => (6, ROL, ZeroPageIndexedWithX(self.next(mem))),
+			0x2e => (6, ROL, Absolute(self.next(mem))),
+			0x3e => (7, ROL, AbsoluteIndexedWithX(self.next(mem))),
+			0x6a => (2, ROR, Accumulator),
+			0x66 => (5, ROR, ZeroPage(self.next(mem))),
+			0x76 => (6, ROR, ZeroPageIndexedWithX(self.next(mem))),
+			0x6e => (6, ROR, Absolute(self.next(mem))),
+			0x7e => (7, ROR, AbsoluteIndexedWithX(self.next(mem))),
+			// Jump & calls
+			0x4c => (3, JMP, Absolute(self.next(mem))),
+			0x6c => (5, JMP, Indirect(self.next(mem))),
+			0x20 => (6, JSR, Absolute(self.next(mem))),
+			0x60 => (6, RTS, Implied),
+			// Branches
+			0x90 => (2, BCC, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0xb0 => (2, BCS, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0xf0 => (2, BEQ, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0x30 => (2, BMI, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0xd0 => (2, BNE, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0x10 => (2, BPL, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0x50 => (2, BVC, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			0x70 => (2, BVS, Relative(self.next(mem))),							// +1 cycle if branched, +2 if page crossed
+			// Status flag changes
+			0x18 => (2, CLC, Implied),
+			0xd8 => (2, CLD, Implied),
+			0x58 => (2, CLI, Implied),
+			0xb8 => (2, CLV, Implied),
+			0x38 => (2, SEC, Implied),
+			0xf8 => (2, SED, Implied),
+			0x78 => (2, SEI, Implied),
+			// System functions
+			0x00 => (7, BRK, Implied),
+			0xea => (2, NOP, Implied),
+			0x40 => (6, RTI, Implied),
+			// Illegal opcode
+			_ => return None,
+		})
 	}
 
 	/// Get the given status flag
@@ -240,9 +413,38 @@ impl CPU<u16> for Mos6502 {
 			debug!("mos6502: IRQ - Jumping to (${:04X}) -> ${:04X}", IRQ_VECTOR, self.pc);
 			return 7;
 		}
+		// Helper function to print a hexdump with up to 4 bytes
+		fn hexdump<M: Addressable<u16>> (mem: &M, addr: u16, addr2: u16) -> ~str {
+			match addr2 - addr {
+				0 => ~"",
+				1 => format!("{:02X}", mem.get(addr)),
+				2 => format!("{:02X} {:02X}", mem.get(addr), mem.get(addr+1)),
+				3 => format!("{:02X} {:02X} {:02X}", mem.get(addr), mem.get(addr+1), mem.get(addr+2)),
+				4 => format!("{:02X} {:02X} {:02X} {:02X}", mem.get(addr), mem.get(addr+1), mem.get(addr+2), mem.get(addr+3)),
+				_ => unreachable!(),
+			}
+		}
+		// Read and parse next opcode
+		let old_pc = self.pc;
+		match self.next_instruction(mem) {
+			// Got valid opcode
+			Some((cycles, instruction, operand)) => {
+				let new_pc = self.pc;
 
-		// TODO...
-		0
+				// TODO: Execute the instruction
+				//instruction.execute(self, mem, operand)
+
+				debug!("mos6502: {:04X}  {:-8s}  {:-3s} {:-26s}  -[{:u}]-> AC:{:02X} X:{:02X} Y:{:02X} SR:{:02X} SP:{:02X} NV-BDIZC:{:08t}",
+					old_pc, hexdump(mem, old_pc, new_pc), instruction.as_str(), operand.as_str(),
+					cycles, self.ac, self.x, self.y, self.sr, self.sp, self.sr);
+				cycles
+			},
+			// Got illegal opcode
+			None => {
+				debug!("mos6502: {:04X}  {:-8s}  ???", old_pc, hexdump(mem, old_pc, old_pc+3));
+				fail!("mos6502: Illegal opcode ${:02X} at ${:04X}", mem.get(old_pc), old_pc)
+			},
+		}
 	}
 }
 
@@ -270,11 +472,19 @@ mod test {
 	fn fetch_memory_contents_and_advance_pc () {
 		let mut cpu = Mos6502::new();
 		let mem = TestMemory;
-		cpu.pc = 0x12;
+		cpu.pc = 0x0012;
 		let val: u8 = cpu.next(&mem); assert_eq!(val, 0x12);
 		let val: u8 = cpu.next(&mem); assert_eq!(val, 0x13);
 		let val: u16 = cpu.next(&mem); assert_eq!(val, 0x1514);
 		let val: u16 = cpu.next(&mem); assert_eq!(val, 0x1716);
+	}
+
+	#[test]
+	fn fetch_instruction_and_advance_pc () {
+		let mut cpu = Mos6502::new();
+		let mut mem = TestMemory;
+		cpu.pc = 0x00ad;					// AD AE AF: LDA $AFAE
+		assert_eq!(cpu.next_instruction(&mut mem), Some((4, LDA, Absolute(0xafae))));
 	}
 
 	#[test]
