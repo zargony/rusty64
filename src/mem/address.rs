59e435b142a3ee58fdfe9b4b79949f6662b4bf16
Andreas Neuhaus <zargony@zargony.com>
2015-10-06 01:46:24 +0200

    Implement successive iterator for addresses

--- src/mem/address.rs
+++ src/mem/address.rs
@@ -6,9 +6,16 @@ use std::{fmt, mem};
 use std::ops::{Not, BitAnd, BitOr, BitXor};
 
 /// A trait for all addresses
-pub trait Address: Copy + Not<Output=Self> + BitAnd<Output=Self> + BitOr<Output=Self> + BitXor<Output=Self> + fmt::UpperHex {
+pub trait Address: Copy + Ord + Eq + Not<Output=Self> + BitAnd<Output=Self> + BitOr<Output=Self> + BitXor<Output=Self> + fmt::UpperHex {
     /// Type of address offset
     type Offset;
+
+    /// The zero address
+    /// TODO: Use std::num::Zero instead when it becomes stable
+    fn zero () -> Self;
+
+    /// The succeeding address (wrapping)
+    fn next (self) -> Self;
 
     /// Calculate new address with given offset (wrapping)
     fn offset (self, offset: Self::Offset) -> Self;
@@ -17,6 +24,11 @@ pub trait Address: Copy + Not<Output=Self> + BitAnd<Output=Self> + BitOr<Output=
     /// the address
     fn offset_masked (&self, offset: Self::Offset, mask: Self) -> Self {
         (*self & mask) | (self.offset(offset) & !mask)
+    }
+
+    /// Return an iterator for getting successive addresses
+    fn successive (self) -> Iter<Self> {
+        Iter { addr: self, done: false }
     }
 
     /// Return an object for displaying the address
@@ -29,6 +41,10 @@ macro_rules! impl_address {
     ($addr_type:ty, $offset_type:ty) => (
         impl Address for $addr_type {
             type Offset = $offset_type;
+
+            fn zero () -> $addr_type { 0 }
+
+            fn next (self) -> $addr_type { self.wrapping_add(1) }
 
             fn offset (self, offset: $offset_type) -> $addr_type {
                 if offset < 0 {
@@ -44,6 +60,27 @@ macro_rules! impl_address {
 impl_address!( u8,  i8);
 impl_address!(u16, i16);
 impl_address!(u32, i32);
+
+/// Iterator for getting successive addresses
+pub struct Iter<A> {
+    addr: A,
+    done: bool,
+}
+
+impl<A: Address> Iterator for Iter<A> {
+    type Item = A;
+
+    fn next (&mut self) -> Option<A> {
+        if self.done {
+            None
+        } else {
+            let addr = self.addr;
+            self.addr = self.addr.next();
+            self.done = self.addr == A::zero();
+            Some(addr)
+        }
+    }
+}
 
 /// Helper struct for displaying an address
 pub struct Display<'a, A: 'a> {
@@ -67,6 +104,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn zero () {
+        assert_eq!(u16::zero(), 0x0000);
+    }
+
+    #[test]
+    fn next () {
+        assert_eq!(0x1234_u16.next(), 0x1235);
+        assert_eq!(0xffff_u16.next(), 0x0000);
+    }
+
+    #[test]
     fn offset () {
         assert_eq!(0x1234_u16.offset( 5), 0x1239);
         assert_eq!(0x1234_u16.offset(-3), 0x1231);
@@ -84,6 +132,15 @@ mod tests {
         assert_eq!(0x12ff_u16.offset_masked( 1, 0xff00), 0x1200);
         assert_eq!(0x1300_u16.offset_masked(-1, 0x0000), 0x12ff);
         assert_eq!(0x1300_u16.offset_masked(-1, 0xff00), 0x13ff);
+    }
+
+    #[test]
+    fn iterating () {
+        let mut it = 0xfffd_u16.successive();
+        assert_eq!(it.next(), Some(0xfffd));
+        assert_eq!(it.next(), Some(0xfffe));
+        assert_eq!(it.next(), Some(0xffff));
+        assert_eq!(it.next(), None);
     }
 
     #[test]
